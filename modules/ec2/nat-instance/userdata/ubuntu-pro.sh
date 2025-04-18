673b06ba25a7f719a83a71141a6538299a5897a7
Evandro Pomatti <evandro.pomatti@gmail.com>
2025-04-17 21:30:54 -0300

    feat:  cloudwatch agent + aws cli

--- modules/ec2/nat-instance/userdata/ubuntu-pro.sh
+++ modules/ec2/nat-instance/userdata/ubuntu-pro.sh
@@ -2,7 +2,40 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-sudo apt update
-sudo apt install -y ubuntu-advantage-tools
-sudo pro enable usg
-sudo apt install -y usg
+### Update ###
+apt update && apt upgrade -y
+
+# CloudWatch Agent
+region=us-east-2
+arch=arm64
+distro=ubuntu
+wget "https://amazoncloudwatch-agent-$region.s3.$region.amazonaws.com/$ubuntu/$arch/latest/amazon-cloudwatch-agent.deb"
+dpkg -i -E ./amazon-cloudwatch-agent.deb
+
+ssmParameterName=AmazonCloudWatch-linux-terraform
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c "ssm:$ssmParameterName"
+
+### NAT ###
+# https://docs.aws.amazon.com/vpc/latest/userguide/VPC_NAT_Instance.html
+sysctl -w net.ipv4.ip_forward=1
+
+# Set the forwarding permanently
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+sysctl -p
+
+apt install -y iptables-persistent
+iptables -t nat -A POSTROUTING -o ens5 -j MASQUERADE
+iptables-save  > /etc/iptables/rules.v4
+
+
+### Ubuntu Pro USG ###
+apt install -y ubuntu-advantage-tools
+pro enable usg
+apt install -y usg
+
+
+### AWS CLI ###
+apt install -y unzip zip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
