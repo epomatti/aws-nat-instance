18ff2a73ab3563f3a5391020cbe10ee705f38c25
Evandro Pomatti <evandro.pomatti@gmail.com>
2025-04-17 21:35:38 -0300

    refactor: removed reboot and debian variable

--- modules/ec2/server/userdata.sh
+++ modules/ec2/server/userdata.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
 export DEBIAN_FRONTEND=noninteractive
-export NEEDRESTART_MODE=a
 
-apt update
-apt upgrade -y
+apt update && apt upgrade -y
 
 apt install -y telnet
-
-
-reboot
